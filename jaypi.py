8a43054862d4da09bab9067a0920a9138d782310
Dylan Ayrey <dxa4481@rit.edu>
2016-04-16 21:28:42 -0400

    initial commit, translated first function

---
+++ jaypi.py
@@ -0,0 +1,64 @@
+import RPIO
+
+RPIO.setmode(RPIO.BOARD)
+
+pins = range(2,12)
+
+RPIO.setup()
+
+"""
+    Once you have found the JTAG pins you can define
+    the following to allow for the boundary scan and
+    irenum functions to be run. Define the values
+    as the index for the pins[] array of the found
+    jtag pin:
+"""
+TCK = 0
+TMS = 1
+TDO = 2
+TDI = 3
+TRST = 4
+
+#Pattern used for scan() and loopback() tests
+pattern_len = 64
+
+pattern = [0, 1, 1, 0, 0, 1, 1, 1, 0, 1, 0, 0, 1, 1, 0, 1, 1, 0, 1, 0, 0, 0, 0, 1, 0, 1, 1, 1, 0, 0, 1, 0, 0, 1]
+
+MAX_DEV_NR = 8
+IDCODE_LEN = 32
+
+SCAN_LEN = 1890
+IR_LEN = 5
+
+IR_IDCODE= [0, 1, 1, 0, 0]
+IR_SAMPLE = [1, 0, 1, 0, 0]
+
+IR_PRELOAD = IR_SAMPLE
+
+TAP_RESET = [1, 1, 1, 1, 1]
+
+TAP_SHIFTDR = [1, 1, 1, 1, 1, 0, 1, 0, 0]
+TAP_SHIFTIR = [1, 1, 1, 1, 1, 0, 1, 1, 0, 0]
+
+IGNOREPIN = 65535
+
+VERBOSE = False
+DELAY = False
+DELAYUS = 5000
+PULLUP = 255
+DEBUGTAP = False
+
+pinslen = len(pins)
+
+def tap_state(tap_state, tck, tms):
+    if DEBUGTAP:
+        print "tap_state: tms set to: "
+    for tap in tap_state:
+        if (DELAY):
+            sleep(0.000005)
+        RPIO.output(tck, 0)
+        RPIO.output(tms, tap)
+        if DEBUGTAP:
+            print(tap)
+        RPIO.output(tck, True)
+
